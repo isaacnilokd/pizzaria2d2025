7770d1719b87ea47edee6521b9a741f68bea121c
isaacnilokd <rm95079@estudante.fieb.edu.br>
2025-03-21 08:19:16 -0300

    projeto pizzaria

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react"
+
+function App(){
+
+  const [nome, setNome] = useState ('SIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU')
+
+  return(
+    <div>
+      <h3>Pizzaria 2D</h3>
+
+      <input 
+        className="nome"
+        onChange={(e)=>{setNome(e.target.value)}}
+        type="text"/>
+        
+      <button
+        className="botao"
+        onClick={()=>{alert(nome)}}
+       >
+        Click Aqui
+
+      </button>
+
+    </div>
+  )
+}
+
+export default App
